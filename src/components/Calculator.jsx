2808dba944c3851c7dc77b012c9ec6bfc358f18b
Het Gurjar <130950604+Het-477@users.noreply.github.com>
2025-05-31 23:41:35 +0530

    feat: add func to handle and evaluate input values on calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { useState } from 'react'
+import { evaluate } from 'mathjs';
 
 const keypadButtons = [
     "7", "8", "9", "/",
@@ -10,23 +11,43 @@ const keypadButtons = [
 
 
 function Calculator() {
-    
+    const [inputValue, setInputValue] = useState("");
+
+    function handleClick(buttonValue) {
+        // console.log(buttonValue);
+        if (buttonValue === 'C') {
+            setInputValue("");
+        }
+        else if (buttonValue === '=') {
+            try {
+                const result = evaluate(inputValue).toString();
+                setInputValue(result);
+            } catch (error) {
+                setInputValue("Error")
+                console.log(error)
+            }
+        }
+        else {
+            setInputValue((prevValue) => prevValue + buttonValue)
+        }
+    }
 
     return (
         <div className="calculator">
-            <div className="display">0</div>
+            <div className="display">{inputValue || 0}</div>
             <div className="keypad-buttons">
                 {keypadButtons.map((buttonValue, index) => {
                     return <button
                         key={index}
                         className={
-                            (buttonValue === 'C' ?
+                            buttonValue === 'C' ?
                                 "clear" :
                                 buttonValue === '=' ?
                                     "equals" :
                                     ['+', '-', '*', '/'].includes(buttonValue) ?
-                                        "operator" : "")
-                        }>
+                                        "operator" : ""
+                        }
+                        onClick={() => handleClick(buttonValue)}>
                         {buttonValue}</button>
                 })}
             </div>
